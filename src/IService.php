862e72087ecf5c3710b24b35aa62c0bb19897eee
TheWhatis <anton-gogo@mail.ru>
2023-11-05 02:49:17 +0700

    Создал интерфейс для реализации методов wb api

---
+++ src/IService.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Файл с интерфейсом сервиса
+ *
+ * PHP version 8
+ *
+ * @category Skeleton
+ * @package  WbApi
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/wb-api-skeleton
+ */
+
+namespace Whatis\WBAPI\Skeleton;
+
+/**
+ * Интерфейс сервиса
+ *
+ * PHP version 8
+ *
+ * @category Skeleton
+ * @package  WbApi
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/wb-api-skeleton
+ */
+interface IService
+{
+    /**
+     * Иницилизация сервиса
+     *
+     * @param string|IClient $tokenOrClient Токен
+     */
+    public function __construct(string|IClient $tokenOrClient);
+}
