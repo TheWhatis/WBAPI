e55942a95852e4fe36bbc02d5582c1ef0e29d840
TheWhatis <anton-gogo@mail.ru>
2023-11-05 02:48:17 +0700

    Создал основной класс для работы с wb api

---
+++ src/Client.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Файл с основным классом
+ * клиента wildberries api
+ *
+ * PHP version 8
+ *
+ * @category Skeleton
+ * @package  WbApi
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/wb-api-skeleton
+ */
+
+namespace Whatis\WBAPI\Skeleton;
+
+/**
+ * Основной класс клиента
+ * wildberries api
+ *
+ * PHP version 8
+ *
+ * @category Skeleton
+ * @package  WbApi
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/wb-api-skeleton
+ */
+class Client extends BaseClient
+{
+    // ...
+}
