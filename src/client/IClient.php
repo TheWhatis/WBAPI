82dc30bf5a5b85be32deb850b604a60fdfa09e13
TheWhatis <anton-gogo@mail.ru>
2023-11-05 02:46:26 +0700

    Создал интерфейс для работы с клиентом wb api

---
+++ src/client/IClient.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Файл с интерфейсом
+ * клиента для wildberries api
+ *
+ * PHP version 8
+ *
+ * @category Skeleton
+ * @package  WbApi
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/wb-api-skeleton
+ */
+
+namespace Whatis\WBAPI\Skeleton;
+
+/**
+ * Интерфейс клиента
+ * для wildberries api
+ *
+ * PHP version 8
+ *
+ * @category Skeleton
+ * @package  WbApi
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/wb-api-skeleton
+ */
+interface IClient
+{
+    /**
+     * Иницилизация клиента
+     *
+     * @param string     $token Токен Wildberries
+     * @param ClientType $type  Тип клиента
+     */
+    public function __construct(string $token, ClientType $type);
+
+    /**
+     * Получить тип клиента
+     *
+     * @return ClientType
+     */
+    public function getType(): ClientType;
+    
+    /**
+     * Выполнить запрос к wb api
+     *
+     * @param string $method Метод
+     * @param string $uri    URI запроса
+     * @param array  $data   Данные
+     *
+     * @return array
+     */
+    public function request(
+        string $method,
+        string $uri,
+        array $data
+    ): array;
+}
