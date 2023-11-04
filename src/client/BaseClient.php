7838cc0803e55ff34087422acd07ca73344c0274
TheWhatis <anton-gogo@mail.ru>
2023-11-05 02:47:50 +0700

    Создал абстрактный класс от 'IClient'

---
+++ src/client/BaseClient.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Файл с абстрактным классом
+ * клиента
+ *
+ * PHP version 8
+ *
+ * @category Skeleton
+ * @package  WbApi
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/wb-api-skeleton
+ */
+
+namespace Whatis\WBAPI\Skeleton;
+
+/**
+ * Абстрактный класс клиента
+ * для wildberries api
+ *
+ * PHP version 8
+ *
+ * @category Skeleton
+ * @package  WbApi
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/wb-api-skeleton
+ */
+abstract class BaseClient implements IClient
+{
+    use TClient;
+}
