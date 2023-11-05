e7876d115e76e73886d8035a7edc463e76feb28e
TheWhatis <anton-gogo@mail.ru>
2023-11-05 23:49:39 +0700

    Создал трейт реализующий 'Countable'

---
+++ src/support/Countable.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Файл с трейтом который
+ * реализует интерфейс
+ * Countable
+ *
+ * PHP version 8
+ *
+ * @category Skeleton
+ * @package  WBAPI
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/WBApiSkeleton
+ */
+
+namespace Whatis\WBAPI\Skeleton\Support;
+
+/**
+ * Трейт реализующий Countable
+ *
+ * Чтобы изменить название свойства,
+ * которое будет использоваться для
+ * работы с массивом (по-умолчанию
+ * \- `array`), необходимо
+ * установить свойство
+ * `$property`
+ *
+ * PHP version 8
+ *
+ * @category Skeleton
+ * @package  WBAPI
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/Settings
+ */
+trait ArrayAccess
+{
+    /**
+     * Стандартное название свойства
+     * для работы с массивом
+     *
+     * @internal
+     *
+     * @var string
+     */
+    private string $_property = 'array';
+
+    // И здесь можете указать своё
+    // свойства с названием $property
+    // protected/public string $property = 'array'
+
+    /**
+     * Установить новое значение
+     *
+     * @return int
+     */
+    public function count(): int
+    {
+        return count($this->{$this->property ?? $this->_property});
+    }
+}
