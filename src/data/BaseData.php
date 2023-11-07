e41521c5e79444388bf86d4a2cb96995f50e1c7d
TheWhatis <anton-gogo@mail.ru>
2023-11-07 18:41:02 +0700

    Переделал абстрактный класс BaseData

--- src/data/BaseData.php
+++ src/data/BaseData.php
@@ -35,16 +35,69 @@ abstract class BaseData implements \ArrayAccess, \Iterator
     use Support\AssocIterator;
 
     /**
+     * Стандартное название свойства
+     * для работы с массивом
+     *
+     * @internal
+     *
+     * @var string
+     */
+    private string $_property = 'data';
+
+    /**
+     * Полученные данные
+     *
+     * @var array
+     */
+    public array $data;
+
+    /**
+     * Контекст вызова
+     *
+     * @var object $context;
+     */
+    public object $context;
+
+    /**
      * Иницилизация данных
      *
      * @param array $options Опции
      */
-    abstract public function __construct(array $options);
+    public function __construct(array $options)
+    {
+        foreach ($options as $key => $value) {
+            $this->$key = $value;
+        }
+
+        $property = $this->property ?? $this->_property;
+        $this->$property = array_map(
+            function ($unit) use ($options) {
+                return $this->wrap($unit, $options);
+            }, $this->data[$property]
+        );
+    }
+
+    /**
+     * Завернуть в "оболочку"
+     * единицу данных
+     *
+     * @param mixed $data    Переданные данные
+     * @param array $options Опции, переданные в __construct
+     *
+     * @return mixed
+     */
+    public function wrap(mixed $data, array $options): mixed
+    {
+        return $data;
+    }
 
     /**
      * Конвертировать в массив
      *
      * @return array
      */
-    abstract public function toArray(): array;
+    public function toArray(): array
+    {
+        return $this->data;
+    }
 }
