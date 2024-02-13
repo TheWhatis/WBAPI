5f3b3d3d25fad012794e9e9375ad3590fc83ec44
TheWhatis <anton-gogo@mail.ru>
2024-02-13 15:32:40 +0700

    Написал трейт с реализацей установки контекста форматирования

---
+++ src/Formatters/WithContextTrait.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Файл с трейтом с реализованным
+ * методом для закодирования переданных
+ * данных в string json или StreamInterface
+ *
+ * PHP version 8
+ *
+ * @category Formatters
+ * @package  WBAPI
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/WBAPI
+ */
+
+namespace Whatis\WBAPI\Formatters;
+
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Трейт с реализованным методом
+ * для закодирования переданных данных
+ * в string json или StreamInterface
+ *
+ * PHP version 8
+ *
+ * @category Formatters
+ * @package  WBAPI
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/WBAPI
+ */
+trait WithContextTrait
+{
+    /**
+     * Контекст
+     *
+     * @var RequestInterface|ResponseInterface|null
+     */
+    public RequestInterface|ResponseInterface|null $context = null;
+
+    /**
+     * Установить контекст выполнения
+     *
+     * @param RequestInterface|ResponseInterface $context Контекст
+     *
+     * @return static
+     */
+    public function withContext(
+        RequestInterface|ResponseInterface $context
+    ): static {
+        $this->context = $context;
+        return $this;
+    }
+}
