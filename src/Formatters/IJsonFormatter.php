a4e6479f3ccf47592ef6a36f2e2a3a29ac608a70
TheWhatis <anton-gogo@mail.ru>
2024-02-13 15:31:20 +0700

    Написал интерфейс форматировщика для работы с ответами и запросами к api

---
+++ src/Formatters/IJsonFormatter.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Файл с интерфейсом форматировщика
+ * ответа запроса, чтобы получить
+ * определённый формат данных
+ *
+ * PHP version 8
+ *
+ * @category Formatters
+ * @package  WBAPI
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/WBAPI
+ */
+
+namespace Whatis\WBAPI\Formatters;
+
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Интерфейс форматировщика ответа
+ * запроса, чтобы получить
+ * определённый формат данных
+ *
+ * PHP version 8
+ *
+ * @category Formatters
+ * @package  WBAPI
+ * @author   Whatis <anton-gogo@mail.ru>
+ * @license  unlicense
+ * @link     https://github.com/TheWhatis/WBAPI
+ */
+interface IJsonFormatter
+{
+    /**
+     * Установить контекст выполнения
+     *
+     * @param RequestInterface|ResponseInterface $context Контекст
+     *
+     * @return static
+     */
+    public function withContext(
+        RequestInterface|ResponseInterface $context
+    ): static;
+
+    /**
+     * Закодировать переданный контент
+     * в строку json или StreamInterface
+     *
+     * Необходим для создания запросов
+     *
+     * @param mixed $content Контент
+     *
+     * @return string|StreamInterface
+     */
+    public function encode(mixed $content): string|StreamInterface;
+
+    /**
+     * Декодировать строку json в
+     * необходимый формат
+     *
+     * @param string $content Контент
+     *
+     * @return mixed
+     */
+    public function decode(string $content): mixed;
+}
