186efaac98a46132d9a51daa9e0d576eb048337e
TheWhatis <anton-gogo@mail.ru>
2024-03-15 10:33:15 +0700

    Урезал исключения

--- src/Exceptions/ServiceAlreadyExists.php
+++ src/Exceptions/ServiceAlreadyExists.php
@@ -14,10 +14,7 @@
 
 namespace Whatis\WBAPI\Exceptions;
 
-use Whatis\WBAPI\ServiceManager;
-
 use Exception;
-use Throwable;
 
 /**
  * Исключение, возникающее когда
@@ -33,59 +30,5 @@ use Throwable;
  */
 class ServiceAlreadyExists extends Exception
 {
-    /**
-     * Название
-     *
-     * @var string
-     */
-    protected string $name;
-
-    /**
-     * Карта
-     *
-     * @var array
-     */
-    protected array $mapping;
-
-    /**
-     * Иницилизация исключения
-     *
-     * @param string     $name     Название неверного сервиса
-     * @param array      $mapping  Карта названий и сервисов
-     * @param ?Throwable $previous Предыдущее исключение
-     */
-    public function __construct(
-        string $name, array $mapping, ?Throwable $previous = null
-    ) {
-        parent::__construct(
-            sprintf(
-                'Service with name \'%s\' in \'%s\' '
-                    . 'already exists',
-                $name, ServiceManager::class
-            ), 0, $previous
-        );
-
-        $this->name = $name;
-        $this->mapping = $mapping;
-    }
-
-    /**
-     * Получить название
-     *
-     * @return string
-     */
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    /**
-     * Получить карту
-     *
-     * @return array
-     */
-    public function getMapping(): array
-    {
-        return $this->mapping;
-    }
+    // ...
 }
--- src/Exceptions/ServiceNotFound.php
+++ src/Exceptions/ServiceNotFound.php
@@ -1,8 +1,7 @@
 <?php
 /**
  * Файл с исключением, возникающим
- * когда сервис не найден в
- * менеджере
+ * когда сервиса не существует
  *
  * PHP version 8
  *
@@ -15,15 +14,11 @@
 
 namespace Whatis\WBAPI\Exceptions;
 
-use Whatis\WBAPI\ServiceManager;
-
 use Exception;
-use Throwable;
 
 /**
- * Исключение, возникающее
- * когда сервис не найден в
- * менеджере
+ * Исключение, возникающее когда
+ * сервиса не существует
  *
  * PHP version 8
  *
@@ -35,58 +30,5 @@ use Throwable;
  */
 class ServiceNotFound extends Exception
 {
-    /**
-     * Название
-     *
-     * @var string
-     */
-    protected string $name;
-
-    /**
-     * Карта
-     *
-     * @var array
-     */
-    protected array $mapping;
-
-    /**
-     * Иницилизация исключения
-     *
-     * @param string     $name     Название неверного сервиса
-     * @param array      $mapping  Карта названий и сервисов
-     * @param ?Throwable $previous Предыдущее исключение
-     */
-    public function __construct(
-        string $name, array $mapping, ?Throwable $previous = null
-    ) {
-        parent::__construct(
-            sprintf(
-                'Service \'%s\' does not exists in \'%s\' ',
-                $name, ServiceManager::class
-            ), 0, $previous
-        );
-
-        $this->name = $name;
-        $this->mapping = $mapping;
-    }
-
-    /**
-     * Получить название
-     *
-     * @return string
-     */
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    /**
-     * Получить карту
-     *
-     * @return array
-     */
-    public function getMapping(): array
-    {
-        return $this->mapping;
-    }
+    // ...
 }
